903ffc68df23cd594831f7436737ea954faf5c61
York Schickl <ys@feinheit.ch>
2021-09-24 10:44:15 +0200

    email backend host provider

---
+++ email_hosts/__init__.py
@@ -0,0 +1,40 @@
+import dj_email_url
+from django.core.mail import get_connection
+from django.core.mail.backends.smtp import EmailBackend
+from django.core.exceptions import ImproperlyConfigured
+from speckenv import env
+
+
+DEFAULT_CONF = "EMAIL_HOSTS"
+
+
+class EmailBackendProvider:
+    def __init__(self, key=None):
+        hosts_config = env(DEFAULT_CONF)
+        self.fallback = get_connection()
+        self.backends = {
+            k: EmailBackend(**dj_email_url.parse(url))
+            for k, url in hosts_config.items()
+        }
+        if key:
+            return self.get_backend(key)
+
+    def get_backend(self, key):
+        if key in self.backends:
+            return self.backends[key]
+        elif self.fallback:
+            return self.fallback
+        else:
+            raise ImproperlyConfigured(
+                "Email backend for key '%s' and fallback not configured." % (key)
+            )
+
+
+BACKENDPROVIDER = EmailBackendProvider()
+
+
+def use_backend(key):
+    if hasattr(BACKENDPROVIDER, "get_backend"):
+        return BACKENDPROVIDER.get_backend(key)
+    else:
+        return EmailBackendProvider(key)
