3362950994a2382c99751f409c52b4942f7e6d2d
York Schickl <ys@feinheit.ch>
2021-09-28 15:51:40 +0200

    use correct email conf

--- email_hosts/__init__.py
+++ email_hosts/__init__.py
@@ -8,13 +8,40 @@ from speckenv import env
 DEFAULT_CONF = "EMAIL_HOSTS"
 
 
+def parse_conf(
+    EMAIL_HOST=None,
+    EMAIL_PORT=None,
+    EMAIL_FILE_PATH=None,
+    EMAIL_HOST_USER=None,
+    EMAIL_HOST_PASSWORD=None,
+    EMAIL_USE_TLS=None,
+    EMAIL_USE_SSL=None,
+    EMAIL_BACKEND=None,
+    EMAIL_SSL_KEYFILE=None,
+    EMAIL_SSL_CERTFILE=None,
+    SERVER_EMAIL=None,
+    DEFAULT_FROM_EMAIL=None,
+):
+    return {
+        "host": EMAIL_HOST,
+        "port": EMAIL_PORT,
+        "username": EMAIL_HOST_USER,
+        "password": EMAIL_HOST_PASSWORD,
+        "use_tls": EMAIL_USE_TLS,
+        "use_ssl": EMAIL_USE_SSL,
+        "ssl_keyfile": EMAIL_SSL_KEYFILE,
+        "ssl_certfile": EMAIL_SSL_CERTFILE,
+    }
+
+
 class EmailBackendProvider:
     def __init__(self, key=None):
         hosts_config = env(DEFAULT_CONF)
         self.fallback = get_connection()
+
         self.backends = (
             {
-                k: EmailBackend(**dj_email_url.parse(url))
+                k: EmailBackend(**parse_conf(**dj_email_url.parse(url)))
                 for k, url in hosts_config.items()
             }
             if hosts_config
