df1e8985f0ca61be20e37fa1e87dc5a9fda77f2f
Matthias Kestenholz <mk@feinheit.ch>
2022-02-03 14:32:24 +0100

    Change the way email backends are constructed; add tests for the actual email which is sent

--- email_hosts/backends.py
+++ email_hosts/backends.py
@@ -2,69 +2,40 @@ from functools import cache
 
 import dj_email_url
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.core.mail import get_connection
 from django.core.mail.backends.smtp import EmailBackend
 
 
-DEFAULT_CONF = "EMAIL_HOSTS"
-
-
-def parse_conf(
-    EMAIL_HOST=None,
-    EMAIL_PORT=None,
-    EMAIL_FILE_PATH=None,
-    EMAIL_HOST_USER=None,
-    EMAIL_HOST_PASSWORD=None,
-    EMAIL_USE_TLS=None,
-    EMAIL_USE_SSL=None,
-    EMAIL_BACKEND=None,
-    EMAIL_SSL_KEYFILE=None,
-    EMAIL_SSL_CERTFILE=None,
-    SERVER_EMAIL=None,
-    DEFAULT_FROM_EMAIL=None,
-):
-    return {
-        "host": EMAIL_HOST,
-        "port": EMAIL_PORT,
-        "username": EMAIL_HOST_USER,
-        "password": EMAIL_HOST_PASSWORD,
-        "use_tls": EMAIL_USE_TLS,
-        "use_ssl": EMAIL_USE_SSL,
-        "ssl_keyfile": EMAIL_SSL_KEYFILE,
-        "ssl_certfile": EMAIL_SSL_CERTFILE,
+def parse_conf(settings):
+    kwargs = {
+        "host": settings["EMAIL_HOST"],
+        "port": settings["EMAIL_PORT"],
+        "username": settings["EMAIL_HOST_USER"],
+        "password": settings["EMAIL_HOST_PASSWORD"],
+        "use_tls": settings["EMAIL_USE_TLS"],
+        "use_ssl": settings["EMAIL_USE_SSL"],
     }
+    if timeout := settings.get("EMAIL_TIMEOUT"):
+        kwargs["timeout"] = timeout
+    if ssl_keyfile := settings.get("EMAIL_SSL_KEYFILE"):
+        kwargs["ssl_keyfile"] = ssl_keyfile
+    if ssl_certfile := settings.get("EMAIL_SSL_CERTFILE"):
+        kwargs["ssl_certfile"] = ssl_certfile
+    return kwargs
 
 
-class EmailBackendProvider:
-    def __init__(self):
-        hosts_config = getattr(settings, DEFAULT_CONF)
-        self.fallback = get_connection()
-
-        self.backends = (
-            {
-                k: EmailBackend(**parse_conf(**dj_email_url.parse(url)))
-                for k, url in hosts_config.items()
-            }
-            if hosts_config
-            else None
-        )
-
-    def get_backend(self, key):
-        if self.backends and key in self.backends:
-            return self.backends[key]
-        elif self.fallback:
-            return self.fallback
-        else:
-            raise ImproperlyConfigured(
-                "Email backend for key '%s' and fallback not configured." % (key)
-            )
+class EmailHostsBackend(EmailBackend):
+    @classmethod
+    def from_dsn(cls, dsn):
+        kwargs = parse_conf(dj_email_url.parse(dsn))
+        default_from_email = kwargs.pop("DEFAULT_FROM_EMAIL", "")
+        backend = cls(**kwargs)
+        backend.default_from_email = default_from_email
+        return backend
 
 
 @cache
-def _provider():
-    return EmailBackendProvider()
-
-
 def use_backend(key):
-    return _provider().get_backend(key)
+    if dsn := settings.EMAIL_HOSTS.get(key):
+        return EmailHostsBackend.from_dsn(dsn)
+    return get_connection()
--- tests/testapp/test_email_hosts.py
+++ tests/testapp/test_email_hosts.py
@@ -1,13 +1,17 @@
-import dj_email_url
-from django.test import TestCase
+from unittest import mock
 
-from email_hosts.backends import parse_conf
+import dj_email_url
+from django.core.mail import EmailMessage
+from django.test import TestCase
+from django.test.utils import override_settings
+
+from email_hosts.backends import parse_conf, use_backend
 
 
 class EmailHostsTest(TestCase):
     def test_parse_conf(self):
         conf = parse_conf(
-            **dj_email_url.parse("submission://USER:PASSWORD@smtp.sendgrid.com")
+            dj_email_url.parse("submission://USER:PASSWORD@smtp.sendgrid.com")
         )
         self.assertEqual(
             conf,
@@ -18,7 +22,44 @@ class EmailHostsTest(TestCase):
                 "password": "PASSWORD",
                 "use_tls": True,
                 "use_ssl": False,
-                "ssl_keyfile": None,
-                "ssl_certfile": None,
             },
         )
+
+    @override_settings(
+        EMAIL_HOSTS={
+            "en": "submission://USER:PASSWORD@smtp.sendgrid.com",
+            "de": "submission://USER:PASSWORD@smtp.mailgun.com",
+        },
+    )
+    def test_use_backend(self):
+        self.assertEqual(use_backend("en").host, "smtp.sendgrid.com")
+        self.assertEqual(use_backend("de").host, "smtp.mailgun.com")
+
+        with mock.patch("smtplib.SMTP", autospec=True) as mock_smtp:
+            EmailMessage(
+                "Hello",
+                "World",
+                to=["recipient@example.com"],
+                connection=use_backend("en"),
+            ).send()
+
+            mock_smtp.assert_called()
+            name, args, kwargs = [
+                call for call in mock_smtp.method_calls if call[0].endswith(".sendmail")
+            ][0]
+            # print(sendmail)
+            print(name, args, kwargs)
+
+            self.assertEqual(args[0], "webmaster@localhost")
+            self.assertEqual(args[1], ["recipient@example.com"])
+
+            lines = args[2].splitlines()
+            self.assertIn(b"From: webmaster@localhost", lines)
+            self.assertIn(b"To: recipient@example.com", lines)
+
+            # context = mock_smtp.return_value.__enter__.return_value
+            # print(context, context.__dict__)
+            # context.ehlo.assert_called()
+            # context.starttls.assert_called()
+            # context.login.assert_called()
+            # context.send_message.assert_called_with(msg)
